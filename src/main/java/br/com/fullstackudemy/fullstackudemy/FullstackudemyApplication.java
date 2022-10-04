045df16c5c5630fae5478a3b7cf2532ea1905cf1
MatheusCostaVaz <matheuscostav@outlook.com>
2022-10-04 20:21:09 -0300

    primeiro commit

---
+++ src/main/java/br/com/fullstackudemy/fullstackudemy/FullstackudemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstackudemy.fullstackudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackudemyApplication.class, args);
+	}
+
+}
